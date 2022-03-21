bbd691efb083de536bf2015b8501151c77cf690d
Mateusz Matysiak <mateuszmatysiak96@gmail.com>
2022-03-21 14:57:39 +0100

    testing changes

--- backend/src/app.middleware.ts
+++ backend/src/app.middleware.ts
@@ -12,41 +12,43 @@ import { PrismaService } from "./prisma.service";
 export class AppMiddleware implements NestMiddleware {
   constructor(private prisma: PrismaService, private appService: AppService) {}
 
-  async initialUser(username: string) {
-    const user = await this.prisma.user.findUnique({ where: { username } });
+  // async initialUser(username: string) {
+  //   const user = await this.prisma.user.findUnique({ where: { username } });
 
-    if (!user) return await this.prisma.user.create({ data: { username } });
-    else return user;
-  }
+  //   if (!user) return await this.prisma.user.create({ data: { username } });
+  //   else return user;
+  // }
 
   async use(req: Request, res: Response, next: NextFunction) {
-    let cachedUser: any = await this.appService.getUserFromMemoryCache();
+    // let cachedUser: any = await this.appService.getUserFromMemoryCache();
 
-    if (!cachedUser) {
-      cachedUser = await fetch(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
-        headers: {
-          Authorization: req.headers.authorization,
-        },
-      }).then(async (res) => {
-        if (res.status === 401) {
-          throw new UnauthorizedException();
-        }
+    // if (!cachedUser) {
+    //   cachedUser = await fetch(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
+    //     headers: {
+    //       Authorization: req.headers.authorization,
+    //     },
+    //   }).then(async (res) => {
+    //     if (res.status === 401) {
+    //       throw new UnauthorizedException();
+    //     }
 
-        const data = await res.json();
-        if (res.ok) {
-          await this.appService.setUserInMemoryCache(data.email);
-          return data;
-        } else {
-          return Promise.reject(data);
-        }
-      });
-    }
+    //     const data = await res.json();
+    //     if (res.ok) {
+    //       await this.appService.setUserInMemoryCache(data.email);
+    //       return data;
+    //     } else {
+    //       return Promise.reject(data);
+    //     }
+    //   });
+    // }
 
-    const user = await this.initialUser(
-      typeof cachedUser === "string" ? cachedUser : cachedUser.email
-    );
+    // const user = await this.initialUser(
+    //   typeof cachedUser === "string" ? cachedUser : cachedUser.email
+    // );
 
-    res.locals.username = await user.username;
+    // res.locals.username = await user.username;
+
+    res.locals.username = "test@gmail.com";
 
     next();
   }
